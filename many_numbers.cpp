23c3e75cf1ecbfdf0d4dbb1c2b406ba42f409ba6
Shivam Kumar <48478625+shivam-kr216@users.noreply.github.com>
2021-01-27 21:37:02 +0530

    Add files via upload

---
+++ many_numbers.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a = (4,6,5);
+    cout<<a<<endl;
+    switch(a){
+        case 2+3: cout<<"5 ";
+        case 3+3: cout<<"6 ";
+        default: cout<<"all ";
+    }
+    return 0;
+}
