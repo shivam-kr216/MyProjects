d3dc6d96aa44320f753826e79ea98ac978f69aed
Shivam Kumar <48478625+shivam-kr216@users.noreply.github.com>
2021-01-29 19:37:07 +0530

    Longest substring

---
+++ longest_substring.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="AGGTAB";
+    string s1="GXTXAYB";
+    int arr[10][10];
+    for(int i=0; i<s1.length()+1; i++){
+        for(int j=0; j<s.length()+1; j++){
+            if(i==0 || j==0){
+                arr[i][j]=0;
+                continue;    
+            }
+            else if(s[j-1]==s1[i-1]){
+                arr[i][j] = arr[i-1][j-1] + 1;
+            }
+            else{
+                arr[i][j] = max(arr[i-1][j], arr[i][j-1]);
+            }
+        }
+        
+    }
+    cout<<arr[s1.length()][s.length()];
+    return 0;
+}
